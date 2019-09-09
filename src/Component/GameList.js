bd7c79c13408ba7c31e53f3b21e7cde54d84bde5
zziqiang <z2206071258@163.com>
2019-09-09 22:07:24 +0800

    first commit

---
+++ src/Component/GameList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default class GameInstall extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                这是游戏下载
+            </div>
+        )
+    }
+}
